95bc9235035027c50f57361d692afa2cf739e1fe
raj swami <er.rajswami@gmail.com>
2020-05-10 18:46:42 +0530

    vuepress setup for documentation

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: 'Eagle.js',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.svg' }]
+    ],
+    dest: 'dist',
+    themeConfig: {
+      logo: '/logo.svg',
+    }
+}
